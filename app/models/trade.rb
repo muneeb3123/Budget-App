d0889bedd3747ed34c7061226a79d553a2214381
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-31 03:18:40 +0500

    add validations

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,4 +2,7 @@ class Trade < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :group, presence: true
 end
