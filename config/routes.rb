b404198c8f68813d25e1cbdb340bcc19656d976a
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-30 10:00:59 +0500

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "homes#index"
   devise_for :users
-  resources :groups, only: [:index, :new, :create, :destroy] do
+  resources :users do
+  resources :groups, only: [:index, :new, :show, :create, :destroy] do
     resources :trades, only: [:index, :new, :create, :destroy]
   end
+end
 end
