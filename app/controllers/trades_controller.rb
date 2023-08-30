df2b458e6d66c9c4ebd622243854aa876c06cfe3
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-30 09:59:19 +0500

    add functionality for transactions

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,28 @@
 class TradesController < ApplicationController
-
-    def index
+    def new
+      @group = Group.find(params[:group_id])
+      @trade = Trade.new
     end
-end
+  
+    def create
+        @group = Group.find(params[:group_id])
+        @trade = @group.trades.new(trade_params)
+        @trade.user_id = current_user.id
+      
+        if @trade.save
+          flash[:notice] = "Transaction created successfully."
+          redirect_to user_group_path(current_user, @group)
+        else
+          flash[:alert] = "Transaction not created. Please check the input fields."
+          render :new
+        end
+      end
+      
+  
+    private
+  
+    def trade_params
+      params.require(:trade).permit(:name, :amount)
+    end
+  end
+  
