b87375180e6fda0e2a6e9c17e56b8bb945ddf935
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-30 09:58:36 +0500

    modify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,6 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-        groups_path
+        user_groups_path(current_user)
       end
-      
 end
