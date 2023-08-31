f76727b186a30b72164e8ec00ed505c7328b8ac7
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-09-01 01:07:02 +0500

    add integeration test for group_index

---
+++ spec/system/group_index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Groups', type: :system do
+  include Devise::Test::IntegrationHelpers
+
+  before do
+    @user = User.create(name: 'muneeb', email: 'muneeb@gmail.com', password: '123456')
+    login_as(@user, scope: :user)
+    @group = Group.create(name: 'School', icon: 'https://img.freepik.com/free-vector/gradient-high-school-logo-design_23-2149626932.jpg', user: @user)
+  end
+
+  describe 'GET index' do
+    it 'displays  categories information ' do
+      visit user_groups_path(user_id: @user.id)
+
+      expect(page).to have_css('img[src="https://img.freepik.com/free-vector/gradient-high-school-logo-design_23-2149626932.jpg"]')
+      expect(page).to have_content(@group.name)
+    end
+
+    it 'displays "Add new group" link' do
+      visit user_groups_path(user_id: @user.id)
+      expect(page).to have_text('Add new')
+    end
+
+    it 'displays total amount ' do
+      visit user_groups_path(user_id: @user.id)
+      expect(page).to have_text('$ 0')
+    end
+  end
+end
