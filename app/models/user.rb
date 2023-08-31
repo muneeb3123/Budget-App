f4f2935247a46f912eb6a1305f1d9a9cf226ad81
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-08-31 10:49:18 +0500

    add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'invalid email format' }
   validates :password, presence: true
 
-  has_many :groups
-  has_many :trades
+  has_many :groups, dependent: :destroy
+  has_many :trades, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
