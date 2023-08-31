7dc36fc25f3b0febb3a5723727165901a63f7955
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-09-01 03:02:03 +0500

    fix linters error

--- spec/system/group_index_spec.rb
+++ spec/system/group_index_spec.rb
@@ -6,14 +6,15 @@ RSpec.describe 'Groups', type: :system do
   before do
     @user = User.create(name: 'muneeb', email: 'muneeb@gmail.com', password: '123456')
     login_as(@user, scope: :user)
-    @group = Group.create(name: 'School', icon: 'https://img.freepik.com/free-vector/gradient-high-school-logo-design_23-2149626932.jpg', user: @user)
+    @group = Group.create(name: 'School',
+                          icon: 'https://example.jpg', user: @user)
   end
 
   describe 'GET index' do
     it 'displays  categories information ' do
       visit user_groups_path(user_id: @user.id)
 
-      expect(page).to have_css('img[src="https://img.freepik.com/free-vector/gradient-high-school-logo-design_23-2149626932.jpg"]')
+      expect(page).to have_css('img[src="https://example.jpg"]')
       expect(page).to have_content(@group.name)
     end
 
@@ -23,9 +24,9 @@ RSpec.describe 'Groups', type: :system do
     end
 
     it 'displays "create" link to create new category' do
-        visit new_user_group_path(user_id: @user.id)
-        expect(page).to have_text('create')
-      end
+      visit new_user_group_path(user_id: @user.id)
+      expect(page).to have_text('create')
+    end
 
     it 'displays total amount ' do
       visit user_groups_path(user_id: @user.id)
--- spec/system/group_show_spec.rb
+++ spec/system/group_show_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe 'Group show', type: :system do
     @user = User.create(name: 'muneeb', email: 'muneeb@gmail.com', password: '123456')
     login_as(@user, scope: :user)
 
-    @group = Group.create(name: 'Mcdonalds', icon: 'https://static.vecteezy.com/system/resources/previews/020/190/610/original/mcdonalds-logo-mcdonald-icon-free-free-vector.jpg', user: @user)
+    @group = Group.create(name: 'Mcdonalds',
+                          icon: 'https://example.jpg', user: @user)
 
     @trade = Trade.create(name: 'Burger', amount: '100', user: @user, group: @group)
-
   end
 
   describe 'GET index' do
     it 'displays  transaction information ' do
       visit user_group_path(user_id: @user.id, id: @group.id)
 
-      expect(page).to have_css('img[src="https://static.vecteezy.com/system/resources/previews/020/190/610/original/mcdonalds-logo-mcdonald-icon-free-free-vector.jpg"]')
+      expect(page).to have_css('img[src="https://example.jpg"]')
       expect(page).to have_content(@trade.name)
     end
 
@@ -32,8 +32,8 @@ RSpec.describe 'Group show', type: :system do
     end
 
     it 'displays amount o transaction' do
-        visit user_groups_path(user_id: @user.id, id: @group.id)
-        expect(page).to have_text('$ 100')
-      end
+      visit user_groups_path(user_id: @user.id, id: @group.id)
+      expect(page).to have_text('$ 100')
+    end
   end
 end
